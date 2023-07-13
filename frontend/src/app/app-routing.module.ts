6c42ffdb5a8e2f29b27e0c9da59377eca4a17047
ffekirnew <ffekirnew0808@gmail.com>
2023-07-13 10:54:12 +0300

    Added another functionality

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from "./components/home-page/home-page.component";
+import { ContentComponent } from './components/content/content.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomePageComponent},
+    // Redirect to home
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '', component: HomePageComponent, children: [
+        {path: 'home', component: ContentComponent},
+        {path: 'contact', component: ContactComponent}
+    ]},
 ];
 
 @NgModule({
--- frontend/src/app/components/info-card/info-card.component.ts
+++ frontend/src/app/components/info-card/info-card.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-info-card',
   templateUrl: './info-card.component.html',
-  styleUrls: ['./info-card.component.scss']
+  styleUrls: ['./info-card.component.scss'],
 })
-export class InfoCardComponent {
+export class InfoCardComponent implements AfterViewChecked {
+  isRouteActive: boolean;
 
+  constructor(private route: ActivatedRoute) {
+    this.isRouteActive = false; // Set initial value
+  }
+
+  ngAfterViewChecked() {
+    console.log('here');
+    this.updateRouteActiveState();
+  }
+
+  // Call this method when the route state changes to update `isRouteActive`
+  updateRouteActiveState() {
+    this.isRouteActive =
+      this.route.snapshot.firstChild?.routeConfig?.path === 'contact';
+  }
 }
